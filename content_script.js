2db13ff3d0a1842dc4bf65a11031ab22e82434e5
Ishibashi Hideto <me@ishibashihideto.net>
2013-10-29 05:15:18 +0900

    stop hiding on Notifications page

--- content_script.js
+++ content_script.js
@@ -7,7 +7,7 @@ var lang = $('html').attr('lang') || 'en';
 var regexp = REGEXP_LANG_TABLE[lang];
 
 setInterval(function () {
-  $('li._33c, li.pvs.notification').each(function () {
+  $('li._33c').each(function () {
     if ( $(this).text().match(regexp) ) {
       $(this).hide();
     }
