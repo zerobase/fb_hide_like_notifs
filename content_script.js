2f4501f7a30a7d513b9ccec72f1de40055a50d74
Ishibashi Hideto <me@ishibashihideto.net>
2013-10-29 15:07:38 +0900

    fix bug: regular expressions to match sentense end variations. (en: /[:.]/, ja: /[:。]/)

--- content_script.js
+++ content_script.js
@@ -1,13 +1,12 @@
 var REGEXP_LANG_TABLE = {
-  en: /^[^:]* likes? your \w+:/g,
-  ja: /^[^:]*について「いいね！」と(言|い)っています:/g
+  en: /^[^:]* likes? your \w+[:.]/g,
+  ja: /^[^:]*について「いいね！」と(言|い)っています[:。]/g
 };
-
-var lang = $('html').attr('lang') || 'en';
-var regexp = REGEXP_LANG_TABLE[lang];
 
 setInterval(function () {
   $('li._33c').each(function () {
+    var lang = $('html').attr('lang') || 'en';
+    var regexp = REGEXP_LANG_TABLE[lang];
     if ( $(this).text().match(regexp) ) {
       $(this).hide();
     }
