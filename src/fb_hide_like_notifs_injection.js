b9878cb4f4f67663e38f8b0d45febf4f258e6aba
Hideto Ishibashi <hidetoi@gmail.com>
2017-10-17 16:30:45 +0900

    Hide ‘highlights_friend_liker_commenter’

--- src/fb_hide_like_notifs_injection.js
+++ src/fb_hide_like_notifs_injection.js
@@ -6,7 +6,8 @@ function markUninterestedNotifsRead () {
   ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"notif_type":"page_new_likes"'] ._55m9._55ma._5c9q,
   ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"notif_type":"page_user_activity"'] ._55m9._55ma._5c9q,
   ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"notif_type":"open_graph_action_like"'] ._55m9._55ma._5c9q,
-  ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"notif_type":"page_fan"'] ._55m9._55ma._5c9q`
+  ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"notif_type":"page_fan"'] ._55m9._55ma._5c9q,
+  ul:not([data-gt*='"ref":"notif_page"']) ._33c[data-gt*='"subtype":"highlights_friend_liker_commenter"'] ._55m9._55ma._5c9q`
 
   var fnSendClickEvent = function (elm) {
     var evt = document.createEvent("MouseEvent")
